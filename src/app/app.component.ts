a09295e97e193e7eb4856323060c08371868330f
xkyns <kayaenes.pro@gmail.com>
2020-08-31 15:46:13 +0200

    ko

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,6 +27,7 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     this.getFriends();
+    this.fbLibrary();
   }
 
   getFriends(){
@@ -36,5 +37,44 @@ export class AppComponent implements OnInit {
         this.friends = response;
       }
     );
+  }
+
+    fbLibrary() {
+      (window as any).fbAsyncInit = function() {
+        window['FB'].init({
+          appId      : '436417747304974',
+          cookie     : true,
+          xfbml      : true,
+          version    : 'v3.1'
+        });
+        window['FB'].AppEvents.logPageView();
+      };
+  
+      (function(d, s, id){
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) {return;}
+        js = d.createElement(s); js.id = id;
+        js.src = "https://connect.facebook.net/en_US/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));
+  }
+
+    login() {
+      window['FB'].login((response) => {
+          console.log('login response',response);
+          if (response.authResponse) {
+  
+            window['FB'].api('/me', {
+              fields: 'last_name, first_name, email'
+            }, (userInfo) => {
+  
+              console.log("user information");
+              console.log(userInfo);
+            });
+            
+          } else {
+            console.log('User login failed');
+          }
+      }, {scope: 'email'});
   }
 }
